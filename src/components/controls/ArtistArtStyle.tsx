7385be22e8c341083fd0ce17000459b00af95903
ares <aresvenus121039@gmail.com>
2024-01-13 09:25:31 +0900

    transform

--- src/components/controls/ArtistArtStyle.tsx
+++ src/components/controls/ArtistArtStyle.tsx
@@ -1,4 +1,4 @@
-import { Chip } from '@mui/material';
+import { Chip,Button } from '@mui/material';
 import * as React from 'react';
 import { FC } from 'react';
 import styled, { css } from 'styled-components';
@@ -21,25 +21,24 @@ const ArtistArtStyle: FC<ArtistArtStyleProps> = ({ artStyles = [] }) => {
   return (
     <Root>
       {artStyles.map((styleName, index) => (
-        <Chip
-          label={styleName}
-          key={index}
-          sx={{
-            fontFamily: 'Roboto',
-            fontSize: '16px',
-            fontStyle: 'normal',
-            fontWeight: '900',
-            lineHeight: '22px',
-            fontVariant: 'all-small-caps',
-            letterSpacing: '2.56px',
-            borderRadius: '8px',
-            padding: '12px 9px',
-            border: '1px solid rgba(255, 255, 255, 0.10)',
-            backgroundColor: 'rgba(255, 255, 255, 0.10)',
-            backdropFilter: 'blur(60px) brightness(100%)',
-            WebkitBackdropFilter: 'blur(60px) brightness(100%)',
-          }}
-        />
+        <Button
+        key={index}
+        sx={{
+          fontFamily: 'Roboto',
+          fontSize: '16px',
+          fontStyle: 'normal',
+          fontWeight: '900',
+          lineHeight: '22px',
+          fontVariant: 'all-small-caps',
+          letterSpacing: '2.56px',
+          borderRadius: '8px',
+          padding: '12px 9px',
+          border: '1px solid rgba(255, 255, 255, 0.10)',
+          backgroundColor: 'rgba(255, 255, 255, 0.10)',
+          backdropFilter: 'blur(60px) brightness(100%)',
+          WebkitBackdropFilter: 'blur(60px) brightness(100%)',
+        }}
+      ><span style={{transform: 'translateY(-1.5px)'}}>{styleName}</span></Button>
       ))}
     </Root>
   );
